5a341ff94c619e914ae76c97e58250acc3b3de0e
Notwacob <wilson.a.jacob@gmail.com>
2023-04-24 16:28:18 -0400

    commit - 4/24/2023

--- portfolio/python/madlibs/random_madlibs.py
+++
@@ -1,6 +0,0 @@
-from sample import madlib, vacation, code, hp, hungergames, zombie
-import random
-
-if __name__ == "__main__":
-    m = random.choice([madlib, vacation, code, hp, hungergames, zombie])
-    m.madlib()
